92512a0866714a53cc5b31cc502a5dda8a979743
star <yanwenz551@gmail.com>
2023-12-17 01:57:10 +0800

    chore(all cpp files): Move all cpp files from / to /cpp feat(rust): Add rust directory

---
+++ rust/test_app/target/debug/deps/test_app-03b8894f26fa86d6.d
@@ -0,0 +1,8 @@
+/home/star/test/rust/test_app/target/debug/deps/test_app-03b8894f26fa86d6.rmeta: src/main.rs Cargo.toml
+
+/home/star/test/rust/test_app/target/debug/deps/test_app-03b8894f26fa86d6.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
---
+++ rust/test_app/target/debug/deps/test_app-ddeb2a673f5d8e82.d
@@ -0,0 +1,8 @@
+/home/star/test/rust/test_app/target/debug/deps/test_app-ddeb2a673f5d8e82.rmeta: src/main.rs Cargo.toml
+
+/home/star/test/rust/test_app/target/debug/deps/test_app-ddeb2a673f5d8e82.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
