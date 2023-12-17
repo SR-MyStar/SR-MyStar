1beb0c9a43c9fc21650417b14cf261d9fb09b7d1
star <yanwenz551@gmail.com>
2023-12-17 15:35:36 +0800

    修改：     .gitignore 	修改：     cpp/test.cpp 	删除：     rust/test_app/target/.rustc_info.json 	删除：     rust/test_app/target/CACHEDIR.TAG 	删除：     rust/test_app/target/debug/.cargo-lock 	删除：     rust/test_app/target/debug/.fingerprint/test_app-03b8894f26fa86d6/dep-test-bin-test_app 	删除：     rust/test_app/target/debug/.fingerprint/test_app-03b8894f26fa86d6/invoked.timestamp 	删除：     rust/test_app/target/debug/.fingerprint/test_app-03b8894f26fa86d6/test-bin-test_app 	删除：     rust/test_app/target/debug/.fingerprint/test_app-03b8894f26fa86d6/test-bin-test_app.json 	删除：     rust/test_app/target/debug/.fingerprint/test_app-3b378c3d546e1535/bin-test_app 	删除：     rust/test_app/target/debug/.fingerprint/test_app-3b378c3d546e1535/bin-test_app.json 	删除：     rust/test_app/target/debug/.fingerprint/test_app-3b378c3d546e1535/invoked.timestamp 	删除：     rust/test_app/target/debug/.fingerprint/test_app-ddeb2a673f5d8e82/bin-test_app 	删除：     rust/test_app/target/debug/.fingerprint/test_app-ddeb2a673f5d8e82/bin-test_app.json 	删除：     rust/test_app/target/debug/.fingerprint/test_app-ddeb2a673f5d8e82/dep-bin-test_app 	删除：     rust/test_app/target/debug/.fingerprint/test_app-ddeb2a673f5d8e82/invoked.timestamp 	删除：     rust/test_app/target/debug/.fingerprint/test_app-fd5866fbaaa5ee61/invoked.timestamp 	删除：     rust/test_app/target/debug/.fingerprint/test_app-fd5866fbaaa5ee61/test-bin-test_app 	删除：     rust/test_app/target/debug/.fingerprint/test_app-fd5866fbaaa5ee61/test-bin-test_app.json 	删除：     rust/test_app/target/debug/deps/libtest_app-03b8894f26fa86d6.rmeta 	删除：     rust/test_app/target/debug/deps/libtest_app-ddeb2a673f5d8e82.rmeta 	删除：     rust/test_app/target/debug/deps/test_app-03b8894f26fa86d6.d 	删除：     rust/test_app/target/debug/deps/test_app-ddeb2a673f5d8e82.d 	删除：     rust/test_app/target/debug/incremental/test_app-2o9y9hs27zuz7/s-grkpxltxxq-mnrxmr-72cb7d9386mzo840mnbed6y6a/dep-graph.bin 	删除：     rust/test_app/target/debug/incremental/test_app-2o9y9hs27zuz7/s-grkpxltxxq-mnrxmr-72cb7d9386mzo840mnbed6y6a/query-cache.bin 	删除：     rust/test_app/target/debug/incremental/test_app-2o9y9hs27zuz7/s-grkpxltxxq-mnrxmr-72cb7d9386mzo840mnbed6y6a/work-products.bin 	删除：     rust/test_app/target/debug/incremental/test_app-2o9y9hs27zuz7/s-grkpxltxxq-mnrxmr.lock 	删除：     rust/test_app/target/debug/incremental/test_app-3cnqynofh4m08/s-grkpxltxwx-1071yg2-ev6bfbhtk9lza2sclw46uz7cs/dep-graph.bin 	删除：     rust/test_app/target/debug/incremental/test_app-3cnqynofh4m08/s-grkpxltxwx-1071yg2-ev6bfbhtk9lza2sclw46uz7cs/query-cache.bin 	删除：     rust/test_app/target/debug/incremental/test_app-3cnqynofh4m08/s-grkpxltxwx-1071yg2-ev6bfbhtk9lza2sclw46uz7cs/work-products.bin 	删除：     rust/test_app/target/debug/incremental/test_app-3cnqynofh4m08/s-grkpxltxwx-1071yg2.lock

--- rust/test_app/target/debug/deps/test_app-03b8894f26fa86d6.d
+++
@@ -1,8 +0,0 @@
-/home/star/test/rust/test_app/target/debug/deps/test_app-03b8894f26fa86d6.rmeta: src/main.rs Cargo.toml
-
-/home/star/test/rust/test_app/target/debug/deps/test_app-03b8894f26fa86d6.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
